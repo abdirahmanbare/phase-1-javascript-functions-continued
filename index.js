ba8f959e85d82f9c0975498f04b9f25460bdbb1b
abdirahmanbare <abdirahmanhusseinbare@gmail.com>
2024-06-17 10:12:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+// Define the saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Test the saturdayFun function
+  console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("bathe my dog")); // "This Saturday, I want to bathe my dog!"
+  
+  // Define the mondayWork function expression
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Test the mondayWork function expression
+  console.log(mondayWork()); // "This Monday, I will go to the office."
+  console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+  
+  // Define the wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Test the wrapAdjective function
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); // "You are !!!a dedicated programmer!!!"
+  
+  const wrapWithPercent = wrapAdjective("%");
+  console.log(wrapWithPercent("a dedicated programmer")); // "You are %a dedicated programmer%!"
+  
